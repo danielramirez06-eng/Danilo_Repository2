2e93d86acc643fad9b2bdd986bb27f0f71b5a3b3
danielramirez06-eng <daniel.ramirez06@uptc.edu.co>
2025-10-25 22:08:23 +0000

    A

--- SegundoTallerDeStringss/hoy.cpp
+++ SegundoTallerDeStringss/hoy.cpp
@@ -1,11 +1,87 @@
 #include <iostream>
 using namespace std;
 
+
+//Punto #3
+
+int contarGuiones(string codigoProducto){
+    int contador = 0;
+    for( int i = 0; i < codigoProducto.size(); i++)
+    {
+       if (codigoProducto[i] == '-')
+       {
+        contador++;
+      }
+    }
+      
+      
+        return contador;
+      
+
+    
+    
+}
+
+int contarDigitos (string codigoProducto){
+    int contador = 0;
+    for(int i = 0; i < codigoProducto.size(); i++)
+    {
+        if (codigoProducto[i] >= '0' && codigoProducto[i] <='9')
+        {
+            contador ++;
+
+        }
+        else
+        {
+            return contador;
+        }
+        
+           
+        
+    }
+    
+}
+
+bool validarUltimaMayuscula (string codigoProducto){
+    char ultima = codigoProducto.size() - 1;
+
+    if(codigoProducto[ultima] >= 'A' && codigoProducto[ultima] <= 'z'){
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+void ejemplo3 (){
+    string hola;
+    cout << "Intresa tu codigo" << endl;
+    cin >> hola;
+
+    cout << "Cantidad de guiones (-) = " << contarGuiones(hola) << endl;
+    cout << "Cantidad digitos (1,2,3,4,5,6,7,8,9) = " << contarDigitos(hola) << endl;
+
+    if (validarUltimaMayuscula(hola)){
+        cout << "El codigo si termina en mayuscula." << endl;
+        
+    }
+    else
+    {
+        cout << "El codigo no cumple con la condicion de la ultima letra en mayuscula." << endl;
+    }
+
+
+}
+
+
 //Punto #2
 
 int contarVocales(string texto) {
     int contador = 0;
-    for (int i = 0; i < texto.size(); i++) {
+    for (int i = 0; i < texto.size(); i++) 
+    {
         char c = texto[i];
         if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
             c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
@@ -17,9 +93,11 @@ int contarVocales(string texto) {
 
 int contarConsonantes(string texto) {
     int contador = 0;
-    for (int i = 0; i < texto.size(); i++) {
+    for (int i = 0; i < texto.size(); i++) 
+    {
         char c = texto[i];
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+         {
              if (!(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
                   c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')) {
                 contador++;
@@ -30,9 +108,11 @@ int contarConsonantes(string texto) {
 }
 int contarNoAlfabeticos(string texto) {
     int contador = 0;
-    for (int i = 0; i < texto.size(); i++) {
+    for (int i = 0; i < texto.size(); i++) 
+    {
         char c = texto[i];
-        if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))) {
+        if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))) 
+        {
             contador++;
         }
     }
@@ -85,6 +165,7 @@ void problema1() {
 
 int main() {
     //problema1();
-    ejemplo2();
+    //ejemplo2();
+    ejemplo3();
     return 0;
 }
